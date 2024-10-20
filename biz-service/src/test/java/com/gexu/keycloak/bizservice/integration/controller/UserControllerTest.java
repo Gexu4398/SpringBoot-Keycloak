344def26fa543ba907829e63ba5e37266999147c
gexu <gexuwork@qq.com>
2024-10-20 16:31:37 +0800

    update: import realm add status

--- biz-service/src/test/java/com/gexu/keycloak/bizservice/integration/controller/UserControllerTest.java
+++ biz-service/src/test/java/com/gexu/keycloak/bizservice/integration/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.gexu.keycloak.bizservice.integration.controller;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,18 +11,23 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.json.JSONUtil;
 import com.gexu.keycloak.bizkeycloakmodel.model.request.NewUserRequest;
 import com.gexu.keycloak.bizkeycloakmodel.model.request.UpdateUserRequest;
+import com.gexu.keycloak.bizkeycloakmodel.service.KeycloakUserService;
 import com.gexu.keycloak.testenvironments.KeycloakIntegrationTestEnvironment;
 import java.util.Set;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 
 @Slf4j
 @WithMockUser(username = "admin", authorities = "user:crud")
 class UserControllerTest extends KeycloakIntegrationTestEnvironment {
+
+  @Autowired
+  private KeycloakUserService keycloakUserService;
 
   @Test
   @SneakyThrows
@@ -97,5 +103,33 @@ class UserControllerTest extends KeycloakIntegrationTestEnvironment {
   @SneakyThrows
   void testGetUser() {
 
+    final var role = dataHelper.newRole(faker.name().title());
+    final var group = dataHelper.newGroup(faker.team().name(), null);
+
+    final var request = new NewUserRequest();
+    request.setUsername(faker.name().firstName());
+    request.setPassword(faker.internet().password());
+    // 接口上使用了@Valid注解对参数进行校验，所以必须全部参数都需要填值
+    request.setName(faker.name().firstName());
+    request.setRoleId(Set.of(role.getId()));
+    request.setGroupId(group);
+    request.setPicture(faker.internet().image());
+    request.setPhoneNumber(faker.phoneNumber().cellPhone());
+
+    mockMvc.perform(post("/user")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JSONUtil.toJsonStr(request)))
+        .andExpect(status().isOk());
+
+    final var user = keycloakUserService.getUsers(null, null, request.getUsername().toLowerCase(),
+        null).getFirst();
+
+    mockMvc.perform(get("/user/" + user.getId()))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.username", equalTo(request.getUsername().toLowerCase())))
+        .andExpect(jsonPath("$.phoneNumber", equalTo(request.getPhoneNumber())))
+        .andExpect(jsonPath("$.picture", equalTo(request.getPicture())))
+        .andExpect(jsonPath("$.role[0].id", equalTo(CollUtil.getFirst(request.getRoleId()))))
+        .andExpect(jsonPath("$.group.id", equalTo(request.getGroupId())));
   }
 }
