27e194d51ec264fbfdd7d5427f874bad8433c3de
gexu <gexuwork@qq.com>
2024-10-20 10:26:03 +0800

    update: pom testcontainers-keycloak version add: .run.xml

---
+++ biz-service/src/test/java/com/gexu/keycloak/bizservice/suite/AllIntegrationTests.java
@@ -0,0 +1,10 @@
+package com.gexu.keycloak.bizservice.suite;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.gexu.keycloak.bizservice.integration")
+public class AllIntegrationTests {
+
+}
